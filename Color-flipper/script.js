13b0bcc4c4bc611eb0da1ef89f0c1beb4285c981
R4NU54 <r4nu54@outlook.es>
2022-12-11 18:16:53 +0100

    Add more colors

--- Color-flipper/script.js
+++ Color-flipper/script.js
@@ -1,15 +1,26 @@
-const colors = ['red', 'green', 'blue', 'yellowgreen', 'aqua']
+const colors = [
+	'Red',
+	'Green',
+	'Blue',
+	'Yellowgreen',
+	'Crimson',
+	'Cyan',
+	'White',
+	'Purple',
+	'Gold',
+	'Orange',
+]
 const btn = document.getElementById('btn')
 const colorTag = document.getElementById('colorTag')
-const main = document.querySelector('.main')
+const main = document.getElementById('main')
 
-btn.addEventListener('click', () => {
+function getRandomColor() {
+	return Math.floor(Math.random() * colors.length)
+}
+
+btn.addEventListener('click', (e) => {
 	const newColor = getRandomColor()
 	main.style.backgroundColor = colors[newColor]
 	colorTag.textContent = colors[newColor]
 	colorTag.style.color = colors[newColor]
 })
-
-function getRandomColor() {
-	return Math.floor(Math.random() * colors.length)
-}
