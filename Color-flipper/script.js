2b917919852ae626dfb7815ad0b5593c420a7261
R4NU54 <r4nu54@outlook.es>
2022-12-13 16:15:34 +0100

    Srcipt V2 add if oldColor === newColor

--- Color-flipper/script.js
+++ Color-flipper/script.js
@@ -1,26 +1,34 @@
 const colors = [
-	'Red',
-	'Green',
-	'Blue',
-	'Yellowgreen',
-	'Crimson',
-	'Cyan',
-	'White',
-	'Purple',
-	'Gold',
-	'Orange',
+	'red',
+	'green',
+	'blue',
+	'yellowgreen',
+	'crimson',
+	'cyan',
+	'white',
+	'purple',
+	'gold',
+	'orange',
 ]
 const btn = document.getElementById('btn')
 const colorTag = document.getElementById('colorTag')
 const main = document.getElementById('main')
 
+function changeColor() {
+	const oldColor = main.style.backgroundColor
+	let newColor = colors[getRandomColor()]
+
+	while (newColor === oldColor) {
+		newColor = colors[getRandomColor()]
+	}
+
+	main.style.backgroundColor = newColor
+	colorTag.textContent = newColor
+	colorTag.style.color = newColor
+}
+
+btn.addEventListener('click', changeColor)
+
 function getRandomColor() {
 	return Math.floor(Math.random() * colors.length)
 }
-
-btn.addEventListener('click', (e) => {
-	const newColor = getRandomColor()
-	main.style.backgroundColor = colors[newColor]
-	colorTag.textContent = colors[newColor]
-	colorTag.style.color = colors[newColor]
-})
