b187cf87a8c33be6728899f8389339a4886a21fc
R4NU54 <r4nu54@outlook.es>
2022-12-16 12:02:46 +0100

    add day3 of Gifts-list

--- Gifts-list/script.js
+++ Gifts-list/script.js
@@ -1,45 +1,37 @@
+// Initial List
+const listOfGifts = ['Ropa Crossfit', 'Monitor Curvo', 'Zapatillas Crossfit']
+
 // SELECT ITEMS
 const giftList = document.getElementById('list')
 const form = document.getElementById('formGift')
 const gift = document.getElementById('inputGift')
 
-// EVENT LISTENERS
+// Event listeners
 form.addEventListener('submit', (e) => {
 	e.preventDefault()
-
 	if (gift.value === '') {
 		form.classList.add('shake')
 		setTimeout(() => {
 			form.classList.remove('shake')
 		}, 500)
 	}
-
 	addItem(gift.value)
-
 	persistItem(gift.value)
 	gift.value = ''
 })
 
-// Initial List
-const listOfGifts = ['Ropa Crossfit', 'Monitor Curvo', 'Zapatillas Crossfit']
-
-// FUNCTIONS
-
+// Functions
 function persistItem(item) {
 	listOfGifts.push(item)
-	console.log(listOfGifts)
 }
-
 function addItem(item) {
 	const newElement = document.createElement('li')
 	newElement.textContent = item
 	giftList.appendChild(newElement)
 }
-
 function initializeList() {
 	listOfGifts.forEach((giftText) => {
 		addItem(giftText)
 	})
 }
-
 initializeList()
